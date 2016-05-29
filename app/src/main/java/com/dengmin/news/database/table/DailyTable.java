6d2ea7add0f54f5604d71650e5066ea5d4bcdc63
coder-dmin <gzdengmin@126.com>
2016-05-29 21:33:44 +0800

    add daily

--- app/src/main/java/com/dengmin/news/database/table/DailyTable.java
+++ app/src/main/java/com/dengmin/news/database/table/DailyTable.java
@@ -1,7 +1,24 @@
 package com.dengmin.news.database.table;
 
 /**
- * Created by dmin on 2016/5/28.
+ * table
  */
 public class DailyTable {
+    public static final String NAME = "daily_table";
+    public static final String COLLECTION_NAME = "collection_daily_table" ;
+
+    public static final String TITLE = "title";
+    public static final String ID = "id";
+    public static final String IMAGE = "image";
+    public static final String BODY = "body";
+    public static final String LARGEPIC = "largepic";
+    public static final String IS_COLLECTED  = "is_collected";
+
+    public static final int ID_TITLE = 0;
+    public static final int ID_ID = 1;
+    public static final int ID_IMAGE = 2;
+    public static final int ID_BODY =3;
+    public static final int ID_LARGEPIC = 4;
+    public static final int ID_IS_COLLECTED = 5;
+
 }
